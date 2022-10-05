f612f507c9804acc45cfa4fea03ef0fef2980358
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-10-05 09:39:41 +0200

    Agregar archivos de proyecto.

---
+++ DEINT-Actividad_Linq_Numeros/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var numeros = Enumerable.Range(1, 100).ToList();
+
+var numDivEntre7 = numeros.Where(x => x % 7 == 0).ToList();
+
+foreach (int i in numDivEntre7)
+{
+    Console.WriteLine(i);
+}
